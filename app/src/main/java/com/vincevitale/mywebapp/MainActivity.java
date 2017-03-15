54f5b6ecf6d792a61048e0a37bc15fb01c521066
Vince Vitale <vincedvitale@gmail.com>
2017-03-15 13:47:05 -0500

    Added MCV debugging code to MainActivity

--- app/src/main/java/com/vincevitale/mywebapp/MainActivity.java
+++ app/src/main/java/com/vincevitale/mywebapp/MainActivity.java
@@ -2,12 +2,63 @@ package com.vincevitale.mywebapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-}
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+} // MainActivity End
+
+
+
+/*if (savedInstanceState != null) {
+    Log.d(TAG, "onCreate() Restoring previous state");
+        *//* restore state *//*
+} else {
+    Log.d(TAG, "onCreate() No saved state available");
+        *//* initialize app *//*
+}*/
